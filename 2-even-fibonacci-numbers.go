64bc4e73bee92bb56291d100a5ad1f56f98931c1
Luis Herrera <luhegoes@gmail.com>
2020-01-10 20:49:54 -0500

    Finished problem 2

---
+++ 2-even-fibonacci-numbers.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func fiboEvenSum(n int) int {
+	sum := 0
+	a := 1
+	b := 1
+	var temp int
+	for b <= 4000000 {
+		temp = a
+		a = b
+		b = a + temp
+
+		if b%2 == 0 {
+			sum += b
+		}
+	}
+
+	return sum
+}
+
+func main() {
+	fmt.Println(fiboEvenSum(43))
+}
